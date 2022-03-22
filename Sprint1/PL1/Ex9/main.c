7f8083a42a0e7abf4003c01a24d78c0d07dab1b8
Pedro Alves <1201381@isep.ipp.pt>
2022-03-22 00:50:10 +0000

    Exercicio 9 - PL1 - Sprint 1

---
+++ Sprint1/PL1/Ex9/main.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main ()
+{
+	pid_t array[10];
+	int i, j;
+	
+	for (i = 0; i < 10; i++){
+		array[i] = fork();
+		
+		if (array[i] == 0){
+			for (j = i*100+1; j < (i+1)*100+1; j++){
+				printf("%d - ", j);
+			}
+			printf("\n");
+			exit(0);
+		}
+	}
+	
+	for (i = 0; i < 10; i++){
+		wait(NULL);
+	}
+				
+} 
+
